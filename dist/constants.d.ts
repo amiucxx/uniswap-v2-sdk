5de80bbb5b7b671932d4ac30081f26ff5fbb51ef
amiucxx <amiucxx@gmail.com>
2021-05-07 18:01:41 +0800

    first commit: modify based on csdn

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x0x0f9860cb0f3057a176712ba0bf6fb95b1a72484f";
+export declare const INIT_CODE_HASH = "0xe018ef94d400537b30c8eff179bbb72754cafbae95301a9ed1c2b73da9ed8623";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
